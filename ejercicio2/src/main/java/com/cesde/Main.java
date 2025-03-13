cd10fc178f0f1fedb47642d3f1447447d73fb03a
Jhon Fredy <jfinfocesde@gmail.com>
2025-03-13 08:55:41 -0500

    Avance en el ejercicio 2

--- ejercicio2/src/main/java/com/cesde/Main.java
+++ ejercicio2/src/main/java/com/cesde/Main.java
@@ -3,5 +3,11 @@ package com.cesde;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println("Hello world!");
+        System.out.println("Hello world!");
+        System.out.println("Hello world!");
+        System.out.println("Hello world!");
+        System.out.println("Hello world!");
+        System.out.println("Hello world!");
     }
 }
